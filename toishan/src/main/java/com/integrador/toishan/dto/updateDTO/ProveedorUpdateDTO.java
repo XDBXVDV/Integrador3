892d36720de1f0941de37b7bd485b473613c05e8
Miguel Valera Solis <solisvaleram@gmail.com>
2026-02-19 21:21:09 -0500

    sadsdasd

--- toishan/src/main/java/com/integrador/toishan/dto/updateDTO/ProveedorUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.integrador.toishan.dto.updateDTO;
-
-public class ProveedorUpdateDTO {
-    private String razonSocial;
-    private String ruc;
-    private String telefono;
-    private String direccion;
-    private String email;
-}
